5349d9cdeab964c7904c68ebc329090096e4d55c
vikash9110 <vikashloveindia@gmail.com>
2020-11-20 18:46:55 +0530

    updated

--- src/main/java/com/vikash/springjpalearning/controller/JPAController.java
+++ src/main/java/com/vikash/springjpalearning/controller/JPAController.java
@@ -1,17 +1,25 @@
 package com.vikash.springjpalearning.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vikash.springjpalearning.db.Student;
+import com.vikash.springjpalearning.db.StudentRepository;
+
 @RestController
 public class JPAController {
-	
-	@Value("${message}")
-	private String message;
+
+	@Autowired
+	StudentRepository studentRepository;
+
 
 	@RequestMapping("/message")
 	public String message() {
-		return this.message;
+		Student student = new Student();
+		student.setName("vikash kumar");
+		studentRepository.save(student);
+		System.out.println("student:  " + studentRepository.save(student).toString());
+		return student.getName();
 	}
 }
---
+++ src/main/java/com/vikash/springjpalearning/db/Student.java
@@ -0,0 +1,29 @@
+package com.vikash.springjpalearning.db;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "student", schema = "public")
+public class Student {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "class")
+	private String grade;
+
+}
